3186c1bacfe7d166b45b788af81424f67c101841
Bulat Cherkashyn <bulatcherkashyn@gmail.com>
2024-06-14 19:21:13 +0300

    init commit

---
+++ .config/nvim/lua/core/settings.lua
@@ -0,0 +1,37 @@
+vim.opt.guicursor = ""
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = false
+vim.opt.smartindent = true
+
+vim.opt.wrap = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
+
+vim.keymap.set("", "<up>", "<nop>", { noremap = true })
+vim.keymap.set("", "<down>", "<nop>", { noremap = true })
+vim.keymap.set("i", "<up>", "<nop>", { noremap = true })
+vim.keymap.set("i", "<down>", "<nop>", { noremap = true })
+vim.opt.mouse = ""
+vim.opt.mousescroll = "ver:0,hor:0"
---
+++ .config/nvim/lua/plugins/init.lua
@@ -0,0 +1,14 @@
+local plugins = {
+	require("plugins.nvim-autopars"),
+	require("plugins.fugfitive"),
+	require("plugins.lualine"),
+	require("plugins.rose-pine"),
+	require("plugins.telescope"),
+	require("plugins.treesiter"),
+	require("plugins.mason"),
+	{
+		"rcarriga/nvim-notify"
+	}
+}
+
+return plugins
